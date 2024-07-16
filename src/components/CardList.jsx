4fedf13cbf9399cbd0b05c7a249ffacf0f00f623
noel <inbox.noel1107@gmail.com>
2024-07-16 21:57:00 +0530

    add json server and proxy

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 function CardList() {
   return (
     <>
-        <section className="py-4">
+        <section className="py-4 px-6">
         <div className="container-xl lg:container m-auto">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
             <Card bg="bg-gray-100">
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,22 +1,45 @@
-import jobs from '../jobs.json'
+import { useState, useEffect } from 'react'
 import JobList from './JobList'
+import Spinner from './Spinner'
 
 const JobListing = ({isHome = true}) => {
-    const showJobs = isHome ? jobs.splice(0,3) : jobs
+    const [jobs,setJobs] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() =>{
+        const fetchJobs = async () => {
+            const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs"
+            try {
+                const response = await fetch(apiUrl)
+                const data = await response.json()
+                setJobs(data)
+            } catch(error) {
+                console.error("error fetching data", error)
+            } finally {
+                 setLoading(false)
+            }
+            
+        }
+        fetchJobs(jobs)
+    }, [])
+
   return (
-    <section className="bg-blue-50 px-4 py-10">
+    <section className="bg-blue-50 px-4 py-10 px-6">
         <div className="container-xl lg:container m-auto">
             <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                 {isHome ? "Recent Jobs" : "Browse Jobs"}
             </h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                {showJobs.map((job) => {
-                    return (
+            {loading ? (<Spinner loading={loading} />)
+            : (
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {jobs.map((job) => 
                         <JobList key={job.id+1} job={job}/>
-                    )
-                })}
+                )}
                 
             </div>
+            )}
+            
+            
         </div>
     </section>
   )
